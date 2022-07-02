a87f4443c60ea77d0b155a324a13f8a60af88376
SofianeIchou <sofiane.ichou.jrbl@gmail.com>
2022-07-03 01:19:56 +0200

    ajout des casquettes

--- utils/data.js
+++ utils/data.js
@@ -39,4 +39,44 @@ export const products = [
     description:
       "Pour commander cette article clique sur Commander Via Whatsapp.",
   },
+  {
+    id: "5",
+    name: "Caquette Karl Bleu Clair",
+    currency: "EUR",
+    price: 30,
+    imageUrl: "/img/karl-bleu.png",
+    images: [],
+    description:
+      "Pour commander cette article clique sur Commander Via Whatsapp.",
+  },
+  {
+    id: "6",
+    name: "Caquette Karl Vert Fluo",
+    currency: "EUR",
+    price: 30,
+    imageUrl: "/img/karl-vert.png",
+    images: [],
+    description:
+      "Pour commander cette article clique sur Commander Via Whatsapp.",
+  },
+  {
+    id: "7",
+    name: "Caquette Karl Jaune",
+    currency: "EUR",
+    price: 30,
+    imageUrl: "/img/karl-jaune.png",
+    images: [],
+    description:
+      "Pour commander cette article clique sur Commander Via Whatsapp.",
+  },
+  {
+    id: "8",
+    name: "Caquette Karl Rouge",
+    currency: "EUR",
+    price: 30,
+    imageUrl: "/img/karl-rouge.png",
+    images: [],
+    description:
+      "Pour commander cette article clique sur Commander Via Whatsapp.",
+  },
 ];
