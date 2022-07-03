2cb5f1044db38aaf9ed6c2b357050e9c7a53ef10
SofianeIchou <sofiane.ichou.jrbl@gmail.com>
2022-07-03 02:26:19 +0200

    whatsapp

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,13 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import { Footer } from '../components/Footer'
-import { WhatsAppChat } from '../components/WhatsappChat'
-
+import { ChakraProvider } from "@chakra-ui/react";
+import { Footer } from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
       <Footer />
-      <WhatsAppChat />
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
